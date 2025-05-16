52351c0d743ea9705435c39405953b3f0d692719
samN00B <sameepaul1212@gmail.com>
2025-05-17 00:34:19 +0530

    vraibles datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "sameerpal12@gmail.com";
+var accountPassword = "12345";
+accountCity = "Muzaffarpur";
+let accountState;
+
+// accountId = 2; //cant be changed
+accountEmail = "samnoob@gmail.com";
+accountPassword = "2121";
+accountCity = "Pune";
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treate all js code as newer version
+
+// alert(3+3); // we are using node js not browser it will throw erre alert is not defined
+
+// numbers => 2 to power 53
+// bigint
+// string => ""
+// boolean => true, false
+// null => standalone value, intentionally declaring variables as null
+// undefined => values not defined yet , variable declared
+// symbol => to find uniqueness
+
+
+// object => {} , can have key and value
+
+// typeof => to find type of an variables
+// console.log(typeof null) => object => This is actually a bug in JavaScript
+// JavaScript values are represented internally as "type tags".
+// null was given a type tag of 0 (same as for objects).
+// So when typeof was implemented, it returned "object" for anything with tag 0.
+// console.log(typeof undefined) => undefined => undefined is a primitive value representing the absence of an assigned value.
+// let a = "sameer"
+// console.log(typeof(a))
+// console.log(typeof a)
+
+// 
+
